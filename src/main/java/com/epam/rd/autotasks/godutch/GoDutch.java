9b12f881fb5de491c1136662c00e29903d7fa03a
Sofiia <sofiakazzz@gmail.com>
2022-10-12 17:32:17 +0300

    The program  equally splits the bill ( plus 10 percent as tips) and prints part to pay for each friend.

--- src/main/java/com/epam/rd/autotasks/godutch/GoDutch.java
+++ src/main/java/com/epam/rd/autotasks/godutch/GoDutch.java
@@ -1,8 +1,28 @@
 package com.epam.rd.autotasks.godutch;
 
+import java.util.Scanner;
+
 public class GoDutch {
 
     public static void main(String[] args) {
         //Write code here
+        Scanner x = new Scanner(System.in);
+        int bill_amount = x.nextInt();
+        int number_of_friends = x.nextInt();
+        int part_of_the_amount = 0;
+        int total_and_tip = bill_amount + (bill_amount / 10);
+
+        if (bill_amount < 0) {
+            System.out.println("Bill total amount cannot be negative");
+        }
+
+        if (number_of_friends <= 0) {
+            System.out.println("Number of friends cannot be negative or zero");
+        }
+        if (bill_amount >= 0 && number_of_friends > 0) {
+            part_of_the_amount = total_and_tip / number_of_friends;
+            System.out.println(part_of_the_amount);
+
+        }
     }
 }
